08226e77e2475e1cc3317ab48cb2ba272a177eaa
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2019-05-10 12:03:00 +0200

    Create Python project related files

---
+++ Python/main.py
@@ -0,0 +1,17 @@
+import os
+import sys
+from PySide2.QtCore import QUrl
+from PySide2.QtQml import QQmlApplicationEngine
+from PySide2.QtWidgets import QApplication
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    engine = QQmlApplicationEngine()
+    engine.addImportPath(os.path.join(os.path.dirname(sys.argv[0]), "..", "Imports"))
+    engine.load(QUrl.fromLocalFile(os.path.join(os.path.dirname(sys.argv[0]), "..", "Qml", "main.qml")))
+
+    if not engine.rootObjects():
+        sys.exit(-1)
+
+    sys.exit(app.exec_())
